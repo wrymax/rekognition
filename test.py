6b1009a73b953ac7a6dd65c20a97664948fcbcb9
王若愚 <wrymax@qq.com>
2017-12-08 12:16:36 +0800

    ~

---
+++ test.py
@@ -0,0 +1,23 @@
+import boto3
+
+if __name__ == "__main__":
+    fileName='svm.png'
+    bucket='ocr-workstream'
+
+    client=boto3.client('rekognition')
+
+    response = client.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': fileName
+            }
+        }, 
+        MinConfidence=75
+    )
+
+    print('Detected labels for ' + fileName)
+    for label in response['Labels']:
+        print (label['Name'] + ' : ' + str(label['Confidence']))
+
+ 
